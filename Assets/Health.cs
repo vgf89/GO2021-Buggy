2ba7b4ec8238b21eb4b15ae68c5d02f9c55495b7
Kevin Hardy <hardy.kevin.c@gmail.com>
2021-11-19 09:55:18 +0900

    Add health script

---
+++ Assets/Health.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private int startingHealth = 5;
+    [ReadOnly] [SerializeField] private int health;
+
+    void Start() {
+        health = startingHealth;
+    }    
+
+    public void takeDamage(int damage) {
+        health -= damage;
+        if (health < 1) {
+            // TODO: Die/respawn, increase frustration stat
+            Debug.Log(name + " has 0 HP");
+        }
+    }
+}
