99feb6432eab7757bbd5e61d07d4c32117453153
Musashi117 <32604297+Musashi117@users.noreply.github.com>
2021-11-23 18:20:24 +0900

    Camera Zoom Function
    This function has been added to the CameraController Script.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,22 +4,37 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public Camera camera;
+
     [Tooltip("Place the Adventurer GameObject here")]
     public Transform adventurerTransform;
-
-    [Tooltip("If following the target, how many seconds until it arrives.")]
-    [MinAttribute(0f)]
-    public float cameraArrivalTime = 0.25f;
+    
 
     private Vector3 cameraVelocity = Vector3.zero;
 
     [Tooltip("Camera offset value from Adventurer about the z-axis")]
     public float cameraOffset;
+    [Tooltip("If following the target, how many seconds until it arrives.")]
+    [MinAttribute(0f)]
+    public float cameraArrivalTime = 0.25f;
+
+    [Header("Camera Zoom")]
+    [SerializeField]
+    [Tooltip("The rate at which the camera zooms relative to deltaTime [HIGHER VALUES RECOMMENDED]")]
+    [MinAttribute(0f)]
+    private float cameraZoomSpeed = 75f;
+    [SerializeField]
+    [MinAttribute(1.0f)]
+    private float cameraMinZoom = 3f;
+    [SerializeField]
+    [MinAttribute(1.0f)]
+    private float cameraMaxZoom = 6f;
+    private float currentZoom;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentZoom = camera.orthographicSize;
     }
 
     // Update is called once per frame
@@ -27,5 +42,18 @@ public class CameraController : MonoBehaviour
     {
         Vector3 destinationPosition = adventurerTransform.position + new Vector3(0,0,cameraOffset);
         transform.position = Vector3.SmoothDamp(transform.position, destinationPosition, ref cameraVelocity, cameraArrivalTime);
+        HandleCameraZoom();
+    }
+
+    void HandleCameraZoom()
+    {
+        if (Input.mouseScrollDelta.y > 0)
+            currentZoom -= cameraZoomSpeed * Time.deltaTime;
+
+        if (Input.mouseScrollDelta.y < 0)
+            currentZoom += cameraZoomSpeed * Time.deltaTime;
+
+        currentZoom = Mathf.Clamp(currentZoom, cameraMinZoom, cameraMaxZoom);
+        camera.orthographicSize = currentZoom;
     }
 }
